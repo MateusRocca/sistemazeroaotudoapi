eb7780874c7187a9e1bfbb53919350b0a8758061
galdi <galdi@DESKTOP-HRAS28U>
2024-05-04 14:26:07 -0300

    Commit incial

---
+++ src/test/java/br/com/rocca/projeto/ProjetoRoccaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rocca.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRoccaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
